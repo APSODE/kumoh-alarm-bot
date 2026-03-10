79b88331415876f92b0f440963d5502b2926c895
APSODE <2gunbo74@gmail.com>
2026-03-10 14:30:54 +0900

    게시글 데이터의 model class

---
+++ src/database/model/ArticleModel.py
@@ -0,0 +1,43 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from database.model.BaseModel import BaseModel
+
+
+class ArticleModel(BaseModel, DeclarativeBase):
+    __tablename__ = "article"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+    title: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(String(255))
+    author: Mapped[str] = mapped_column(String(32))
+    type: Mapped[str] = mapped_column(String(32))
+    article_time: Mapped[str] = mapped_column(String(32))
+
+    def __init__(self, title: str, description: str, author: str, type: str, article_time: str):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.author = author
+        self.type = type
+        self.article_time = article_time
+
+    @staticmethod
+    def create_model(title: str, description: str, author: str, type: str, article_time: str) -> "ArticleModel":
+        return ArticleModel(
+            title = title,
+            description = description,
+            author = author,
+            type = type,
+            article_time = article_time
+        )
+
+
+
+
+
+
+
+
+
+
